652023f83d91d6863d9635e1c809baec20809a18
developer-shahidul <developer-shahidul@outlook.com>
2026-01-01 12:08:19 +0600

    vercel.json update

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-//
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
@@ -13,7 +10,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.39yqdr4.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,136 +20,109 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     const UsersEquipment = client.db("Equipments").collection("items");
     const userConnection = client.db("Equipments").collection("users");
 
     app.get("/items", async (req, res) => {
-      const cursor = UsersEquipment.find();
-      const result = await cursor.toArray();
+      const result = await UsersEquipment.find().toArray();
       res.send(result);
     });
 
     app.post("/items", async (req, res) => {
-      const body = req.body;
-      const result = await UsersEquipment.insertOne(body);
-      res.send(result);
-    });
-    app.delete("/items/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await UsersEquipment.deleteOne(query);
+      const result = await UsersEquipment.insertOne(req.body);
       res.send(result);
     });
 
-    // update
+    app.delete("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await UsersEquipment.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
     app.get("/items/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await UsersEquipment.findOne(query);
+      const result = await UsersEquipment.findOne({
+        _id: new ObjectId(id),
+      });
       res.send(result);
     });
 
     app.put("/items/:id", async (req, res) => {
       const id = req.params.id;
       const body = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const option = { upsert: true };
-      const updateDoc = {
-        $set: {
-          name: body.name,
-          category: body.category,
-          price: body.price,
-          description: body.description,
-          customization: body.customization,
-          processingTime: body.processingTime,
-          quentity: body.quentity,
-          photo: body.photo,
-          rating: body.rating,
-        },
-      };
-      const result = await UsersEquipment.updateOne(filter, updateDoc, option);
+      const result = await UsersEquipment.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: body },
+        { upsert: true }
+      );
       res.send(result);
     });
 
-    // email maching gulue daw
-    // get items only for a specific email
     app.get("/items/user/:email", async (req, res) => {
       const email = req.params.email;
-      const items = await UsersEquipment.find({ email }).toArray();
-      res.send(items);
+      const result = await UsersEquipment.find({ email }).toArray();
+      res.send(result);
     });
 
-    // like aer jonno
     app.patch("/items/like/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-
       const { email } = req.body;
-      const item = await UsersEquipment.findOne(query);
+
+      const item = await UsersEquipment.findOne({
+        _id: new ObjectId(id),
+      });
 
       if (!item) {
-        return res.status(404).send({ message: "items not found" });
+        return res.status(404).send({ message: "Item not found" });
       }
 
       if (item?.likedBy?.includes(email)) {
-        const updateDoc = {
-          $pull: { likedBy: email },
-          $inc: { likes: -1 },
-        };
-        const result = await UsersEquipment.updateOne(query, updateDoc);
-        res.send({ liked: false, result });
+        await UsersEquipment.updateOne(
+          { _id: new ObjectId(id) },
+          { $pull: { likedBy: email }, $inc: { likes: -1 } }
+        );
+        res.send({ liked: false });
       } else {
-        const updateDoc2 = {
-          $addToSet: { likedBy: email },
-          $inc: { likes: 1 },
-        };
-        const result = await UsersEquipment.updateOne(query, updateDoc2);
-        res.send({ liked: true, result });
+        await UsersEquipment.updateOne(
+          { _id: new ObjectId(id) },
+          { $addToSet: { likedBy: email }, $inc: { likes: 1 } }
+        );
+        res.send({ liked: true });
       }
     });
 
-    // user aer jonno
-
     app.get("/users", async (req, res) => {
-      const query = userConnection.find();
-      const result = await query.toArray();
+      const result = await userConnection.find().toArray();
       res.send(result);
     });
 
     app.post("/users", async (req, res) => {
-      const body = req.body;
-      const result = await userConnection.insertOne(body);
+      const result = await userConnection.insertOne(req.body);
       res.send(result);
     });
 
     app.patch("/users", async (req, res) => {
       const email = req.body.email;
-      const query = { email };
-      const userDoc = {
-        $set: { lastSignInTime: req.body?.lastSignInTime },
-      };
-      const result = await userConnection.updateOne(query, userDoc);
+      const result = await userConnection.updateOne(
+        { email },
+        { $set: { lastSignInTime: req.body.lastSignInTime } }
+      );
       res.send(result);
     });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  } catch (error) {
+    console.error(error);
   }
 }
-run().catch(console.dir);
+
+run();
 
 app.get("/", (req, res) => {
   res.send("server site successfully run");
 });
 
-app.listen(port, () => {
-  console.log(`server site is running on port : ${port}`);
-});
+// ✅ VERY IMPORTANT FOR VERCEL
+module.exports = app;
